efede90519b82f040ce17b984cab73750df8160e
huaxiaos <huaxiaos@163.com>
2017-12-25 17:26:48 +0800

    增加GsonUtils

---
+++ app/src/main/java/com/huaxiao/demo/utils/GsonUtils.java
@@ -0,0 +1,27 @@
+package com.huaxiao.demo.utils;
+
+import com.alibaba.fastjson.JSON;
+
+import java.util.List;
+
+/**
+ * Description Gson解析工具类
+ * Author sunhuaxiao
+ * Date 2017/11/7
+ */
+
+public class GsonUtils {
+
+    public static <T> T parseObject(String text, Class<T> clazz) {
+        return JSON.parseObject(text, clazz);
+    }
+
+    public static <T> List<T> parseArray(String text, Class<T> clazz) {
+        return JSON.parseArray(text, clazz);
+    }
+
+    public static String parseObject(Object o) {
+        return JSON.toJSONString(o);
+    }
+
+}
