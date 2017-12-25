4f6ab8ee6d5957d04f8e69354ceb11281a9188dc
huaxiaos <huaxiaos@163.com>
2017-12-25 18:17:19 +0800

    updata ImageUtils

--- app/src/main/java/com/huaxiao/demo/utils/ImageUtils.java
+++ app/src/main/java/com/huaxiao/demo/utils/ImageUtils.java
@@ -9,6 +9,7 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.GlideException;
 import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
 
 import java.io.File;
@@ -50,6 +51,23 @@ public class ImageUtils {
     public static void displayImg(Context context, File file, ImageView imageView) {
         Glide.with(context)
                 .load(file)
+                .into(imageView);
+    }
+
+    /**
+     * 该方法中url可以是下载链接，不需要已.png结尾
+     * @param context Context
+     * @param url url
+     * @param imageView imageView
+     * @param errorImg errorImg
+     */
+    public static void displayImgByDownload(Context context, String url, final ImageView imageView, final int errorImg) {
+        RequestOptions options = new RequestOptions();
+        options.error(errorImg);
+
+        Glide.with(context).asBitmap()
+                .load(url)
+                .apply(options)
                 .into(imageView);
     }
 
