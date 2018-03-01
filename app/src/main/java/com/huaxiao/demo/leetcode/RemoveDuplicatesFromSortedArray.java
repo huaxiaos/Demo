b8772e60787ca0d5cd18fc5dd96c2993ec209e3a
huaxiaos <huaxiaos@163.com>
2018-03-01 17:16:23 +0800

    leetcode RemoveDuplicatesFromSortedArray

---
+++ app/src/main/java/com/huaxiao/demo/leetcode/RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,40 @@
+package com.huaxiao.demo.leetcode;
+
+/**
+ * Description TODO
+ * Author sunhuaxiao
+ * Date 2018/2/28
+ */
+
+public class RemoveDuplicatesFromSortedArray {
+
+    public static int removeDuplicates(int[] nums) {
+//        if (nums == null) return 0;
+//
+//        int temp = 0;
+//        int sum = 0;
+//        int size = nums.length;
+//
+//        for (int i = 0; i < size; i++) {
+//            if (i == 0) {
+//                temp = nums[i];
+//                continue;
+//            }
+//
+//            if (temp == nums[i]) {
+//                sum++;
+//                continue;
+//            }
+//
+//            temp = nums[i];
+//        }
+//
+//        return size - sum;
+
+        int[] test = new int[]{1, 1, 2};
+
+
+        return 0;
+    }
+
+}
--- app/src/main/java/com/huaxiao/demo/leetcode/TwoSum.java
+++ app/src/main/java/com/huaxiao/demo/leetcode/TwoSum.java
@@ -1,10 +1,6 @@
 package com.huaxiao.demo.leetcode;
 
 import android.support.annotation.Nullable;
-import android.support.test.runner.AndroidJUnit4;
-
-import org.junit.Test;
-import org.junit.runner.RunWith;
 
 import java.util.HashMap;
 
@@ -14,10 +10,8 @@ import java.util.HashMap;
  * Date 2018/2/23
  */
 
-@RunWith(AndroidJUnit4.class)
 public class TwoSum {
 
-    @Test
     public void solution() {
         int[] a = new int[]{2, 7, 11, 19};
         int target = 9;
