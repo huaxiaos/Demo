1e80dc1187f6abd45bde618bf6aed12634aab5eb
huaxiaos <huaxiaos@163.com>
2018-03-28 21:48:33 +0800

    增加mvvm

---
+++ app/src/main/java/com/huaxiao/demo/mvvm/MVVMActivity.java
@@ -0,0 +1,15 @@
+package com.huaxiao.demo.mvvm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.huaxiao.demo.R;
+
+public class MVVMActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mvvm);
+    }
+}
---
+++ app/src/main/java/com/huaxiao/demo/mvvm/MVVMViewModel.java
@@ -0,0 +1,10 @@
+package com.huaxiao.demo.mvvm;
+
+/**
+ * Description TODO
+ * Author sunhuaxiao
+ * Date 2018/3/28
+ */
+
+public class MVVMViewModel {
+}
