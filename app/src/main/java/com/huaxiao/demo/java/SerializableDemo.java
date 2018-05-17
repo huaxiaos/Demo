69268da26a2b99c544dcb9b123ac72468fdf1bbe
huaxiaos <huaxiaos@163.com>
2018-05-17 16:37:21 +0800

    add SortColors

---
+++ app/src/main/java/com/huaxiao/demo/java/SerializableDemo.java
@@ -0,0 +1,14 @@
+package com.huaxiao.demo.java;
+
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+/**
+ * Description TODO
+ * Author sunhuaxiao
+ * Date 2018/5/16
+ */
+
+public class SerializableDemo implements Serializable {
+
+}
