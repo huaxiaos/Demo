719aab1b0569f1f2505b7d5d8bdaf680ae42b440
huaxiaos <huaxiaos@163.com>
2018-10-23 16:25:46 +0800

    change compile to implementation

--- app/src/main/java/com/huaxiao/demo/MainActivity.java
+++ app/src/main/java/com/huaxiao/demo/MainActivity.java
@@ -2,28 +2,13 @@ package com.huaxiao.demo;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 
-import com.huaxiao.demo.annotation.AnnotationDemo;
-import com.huaxiao.demo.aspectj.AspectJ1;
 import com.huaxiao.demo.base.BaseActivity;
-import com.huaxiao.demo.bitmap.Bitmap1Activity;
-import com.huaxiao.demo.classloader.ClassLoader;
+import com.huaxiao.demo.exception.DemoException;
 import com.huaxiao.demo.fragment.FragmentDemoActivity;
-import com.huaxiao.demo.handler.HandlerTestActivity;
-import com.huaxiao.demo.java.SynchronizedDemo;
-import com.huaxiao.demo.mvvm.MVVMActivity;
-import com.huaxiao.demo.proxy.ProxyDemoActivity;
-import com.huaxiao.demo.scan.Scan2Activity;
-import com.huaxiao.demo.scan.Scan3Activity;
-import com.huaxiao.demo.scan.ScanActivity;
-import com.huaxiao.demo.service.DemoService;
 import com.huaxiao.demo.spi.SPIDemo;
-import com.huaxiao.demo.spider.SpiderActivity;
-
-import static android.content.Intent.FLAG_INCLUDE_STOPPED_PACKAGES;
 
 public class MainActivity extends BaseActivity implements View.OnClickListener {
 
@@ -42,19 +27,11 @@ public class MainActivity extends BaseActivity implements View.OnClickListener {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.button:
-                Intent intent = new Intent(this, FragmentDemoActivity.class);
-                startActivity(intent);
-//                ClassLoader.main(null);
-
-//                AspectJ1 aspectJ1 = new AspectJ1();
-//                aspectJ1.run();
-
-//                Intent intent = new Intent();
-//                intent.setAction("com.huaxiao.demo.test");
-//                sendBroadcast(intent);
-
-//                Intent intent = new Intent(this, DemoService.class);
-//                startService(intent);
+                try {
+                    DemoException.run();
+                } catch (Exception t) {
+                    t.printStackTrace();
+                }
 
                 break;
 
---
+++ app/src/main/java/com/huaxiao/demo/exception/DemoException.java
@@ -0,0 +1,12 @@
+package com.huaxiao.demo.exception;
+
+/**
+ * Created by sunhuaxiao on 2018/10/23.
+ */
+public class DemoException {
+
+    public static void run() {
+        throw new IllegalArgumentException("DemoException");
+    }
+
+}
